143d9d9bc21badc611c654cbe843b1fba76b7728
love-is-real <cvictoriano@gmail.com>
2024-08-01 21:59:38 -0400

    mi primer commit

---
+++ aplicacion.js
@@ -0,0 +1,15 @@
+
+
+function AgregaTexto (indicador , texto){
+
+    let titulo = document.querySelector(indicador);
+    titulo.innerHTML = texto;
+}
+
+AgregaTexto ('h1','Hola Mundo');
+AgregaTexto ('h2','Segundo Título');
+
+
+
+
+
