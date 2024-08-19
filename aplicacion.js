71d0726e880928cf5884fa4189be206f742b6f55
love-is-real <cvictoriano@gmail.com>
2024-08-18 20:32:13 -0400

    mas funciones

--- aplicacion.js
+++ aplicacion.js
@@ -1,4 +1,4 @@
-
+//funciones de click y Agregar texto
 
 function AgregaTexto (indicador , texto){
 
@@ -9,8 +9,66 @@ function AgregaTexto (indicador , texto){
 AgregaTexto ('h1','Hola Mundo');
 AgregaTexto ('h2','Segundo Título');
 
+//Funcion para calcular el indice de masa corporal.
+
+/*function calculaImc(peso,altura){
+    let indiceMasa = peso / altura ** 2;
+    alert(`Tu indice de masa corporal es ${indiceMasa}`);
+    return
+
+}
+*/
+//practicando con alura funciones y listas
+
+let ListaGenerica = [];
+let lenguajesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
 
 
+function agregando () {
+            
+            lenguajesDeProgramacion.push('Java', 'Ruby','GoLang');
+            console.log(lenguajesDeProgramacion);
+
+}
+
+agregando();
+
+
+// crear una funcion que muestre los elementos separadamente
+
+function agregarSeparadamente () {
+            for (let i = 0 ; i < lenguajesDeProgramacion.length ; i ++){
+                console.log(lenguajesDeProgramacion[i]);
+            }
+}
+
+agregarSeparadamente();
+
+// ahora lista los elementos de manera inversa
+
+function agregarSentidoInverso() {
+            for (let i = lenguajesDeProgramacion.length - 1 ; i >= 0; i--){
+                console.log(lenguajesDeProgramacion[i]);
+            }
+
+}
+
+agregarSentidoInverso();
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
